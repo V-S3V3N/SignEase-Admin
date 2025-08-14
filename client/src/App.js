e0f7121d43583c24e8f592c0966302f8567fbd6c
V-S3V3N <107683720+V-S3V3N@users.noreply.github.com>
2025-08-14 20:19:50 +0800

    publish

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,12 @@
-import React from "react";
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-  Outlet,
-} from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Signin from "./pages/Signin";
-import Layout from "./components/Layout";
-import SubscriptionPlan from "./pages/SubscriptionPlan";
-import Course from "./pages/Course";
-import UserManagement from "./pages/UserManagement";
-import Report from "./pages/Report";
+import React from 'react';
+import { HashRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Signin from './pages/Signin';
+import Layout from './components/Layout';
+import SubscriptionPlan from './pages/SubscriptionPlan';
+import Course from './pages/Course';
+import UserManagement from './pages/UserManagement';
+import Report from './pages/Report';
 
 const PrivateRoute = () => {
   const uid = localStorage.getItem("uid");
@@ -25,15 +19,15 @@ function App() {
       <Routes>
         <Route path="/signin" element={<Signin />} />
 
-        <Route element={<PrivateRoute />}>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/subscriptionPlan" element={<SubscriptionPlan />} />
-            <Route path="/course" element={<Course />} />
-            <Route path="/userManagement" element={<UserManagement />} />
-            <Route path="/report" element={<Report />} />
-          </Route>
-        </Route>
+  <Route element={<PrivateRoute />}>
+    <Route element={<Layout />}>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/subscriptionPlan" element={<SubscriptionPlan />} />
+      <Route path="/course" element={<Course />} />
+      <Route path="/userManagement" element={<UserManagement />} />
+      <Route path="/report" element={<Report />} />
+    </Route>
+  </Route>
       </Routes>
     </Router>
   );
